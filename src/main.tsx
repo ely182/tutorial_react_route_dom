8d65591609e1aa61a1551601a29c12f4955d6e12
ely182 <ely.18gabriel@gmail.com>
2022-12-05 10:45:58 -0300

    a vs link

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { Home } from './components/Home'
 import './styles/global.css';
 
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { About,} from './components/Home/About';
+import { Menu } from './components/Menu';
+
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home/>
+<BrowserRouter>
+<Menu />
+<Routes>
+  <Route path="/" element={    <Home/>}  />
+  <Route path="/about" element={    <About/>}  />
+</Routes>
+</BrowserRouter>
   </React.StrictMode>
-)
+);
